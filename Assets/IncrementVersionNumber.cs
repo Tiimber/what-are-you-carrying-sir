6e6a3ebe6696619dd380487987b10d51eeaa21ec
Anders Ive <anders.ive@gmail.com>
2020-08-25 08:39:36 +0200

    All assets

---
+++ Assets/IncrementVersionNumber.cs
@@ -0,0 +1,39 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEngine;
+
+public class IncrementVersionNumber
+{
+    [MenuItem("Exoa/Build/Increment version")]
+    public static void IncrementVersion()
+    {
+        string version = PlayerSettings.bundleVersion;
+        string[] parts = version.Split('.');
+        int lastNum = int.Parse(parts[parts.Length - 1]);
+        lastNum++;
+        string newVersion = "";
+        for (int i = 0; i < parts.Length - 1; i++)
+            newVersion += parts[i] + ".";
+        newVersion += lastNum;
+        int newVersionCode = int.Parse(newVersion.Replace(".", ""));
+        Debug.Log("IncrementVersion " + version + " " + newVersion + " " + newVersionCode);
+        PlayerSettings.bundleVersion = newVersion;
+        PlayerSettings.Android.bundleVersionCode = newVersionCode;
+    }
+
+    [PostProcessBuild(1080)]
+    public static void OnPostProcessBuild(BuildTarget target, string path)
+    {
+        Debug.Log("OnPostProcessBuild  " + target + " " + path);
+        IncrementVersion();
+    }
+
+    //public class MyBuildPostprocessor {
+    //    [PostProcessBuildAttribute(1)]
+    //    public static void OnPostprocessBuild(BuildTarget target, string pathToBuiltProject) {
+    //        Debug.Log( pathToBuiltProject );
+    //    }
+    //}
+}
+#endif
---
+++ Assets/Test.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class Test : MonoBehaviour
+{
+    public TMPro.TextMeshPro t;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //TMPro.TextMeshPro t = GameObject.AddComponent<TMPro.TextMeshPro>();
+        //t.text = "HEJSAN!";
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
